3191b623c2463a9c523cc62a9c08b25ee0130f3d
gyumin_han <rbals0445@naver.com>
2023-11-24 03:17:55 +0900

    feat: input, textarea는 selectionStart,End로 시작,끝 위치 조절 가능

--- contentScript.js
+++ contentScript.js
@@ -6,26 +6,30 @@ document.addEventListener("keydown", function (event) {
     if (selectedString.length === 0) {
       return;
     }
-    // anchorNode가 input이나 textarea가 아닌 wrapperNode를 가리킨다.
-    const $inputNode = getInputNode(selection.anchorNode);
 
-    const replaceText = "hello World!";
+    const $inputNode = document.activeElement; // 현재 활성화된 요소를 가져옴
 
-    if (selection.rangeCount) {
-      const curText = $inputNode.value;
-      $inputNode.value = curText.replace(selectedString, replaceText);
+    // 요소가 input 또는 textarea인지 확인
+    if ($inputNode.tagName === "INPUT" || $inputNode.tagName === "TEXTAREA") {
+      const start = $inputNode.selectionStart; // 선택 시작 위치
+      const end = $inputNode.selectionEnd; // 선택 끝 위치
+
+      if (start !== end) {
+        // 텍스트가 선택되었는지 확인
+        const replaceText = "hello World!";
+        const curText = $inputNode.value;
+
+        $inputNode.value =
+          curText.substring(0, start) + replaceText + curText.substring(end);
+
+        $inputNode.selectionStart = $inputNode.selectionEnd =
+          start + replaceText.length; // 커서 위치를 변경된 텍스트 끝으로 이동
+      }
     }
   }
 });
 
-function getInputNode($parentNode) {
-  const children = $parentNode.children;
-
-  for (const child of children) {
-    if (child.tagName === "INPUT" || child.tagName === "TEXTAREA") {
-      return child;
-    }
-  }
-
-  return null;
-}
+/**
+ * TODO.
+ * - chrome localstorage에서 등록된 key에 매칭되는지 확인 (확인되면 해당 텍스트로 replace)
+ */
