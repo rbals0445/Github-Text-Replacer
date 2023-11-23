f6283604638e9f3cd962b5e98af5c63300af5bbc
gyumin_han <rbals0445@naver.com>
2023-11-24 02:43:06 +0900

    feat: input, textarea 찾아서 텍스트 치환하기

--- contentScript.js
+++ contentScript.js
@@ -1,9 +1,31 @@
 document.addEventListener("keydown", function (event) {
-  console.log(window.getSelection());
-  console.log(window.getSelection().toString());
-  if (event.key === "a") {
-    document.body.style.backgroundColor = "red";
-  } else {
-    document.body.style.backgroundColor = "green";
+  if (event.key === ".") {
+    const selection = window.getSelection();
+    const selectedString = selection.toString();
+
+    if (selectedString.length === 0) {
+      return;
+    }
+    // anchorNode가 input이나 textarea가 아닌 wrapperNode를 가리킨다.
+    const $inputNode = getInputNode(selection.anchorNode);
+
+    const replaceText = "hello World!";
+
+    if (selection.rangeCount) {
+      const curText = $inputNode.value;
+      $inputNode.value = curText.replace(selectedString, replaceText);
+    }
   }
 });
+
+function getInputNode($parentNode) {
+  const children = $parentNode.children;
+
+  for (const child of children) {
+    if (child.tagName === "INPUT" || child.tagName === "TEXTAREA") {
+      return child;
+    }
+  }
+
+  return null;
+}
